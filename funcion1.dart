ea5a3806d9f2e766fa71023ffc54918ffe696707
Manuel Nava <navaweb@cbtis128.edu.mx>
2025-02-07 16:47:42 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,20 @@
+// funcion que no devuelva valor
+// funcion saludar
+void saludar(){
+  print('Hola mundo');
+}
+
+void escribeNombre(){
+  print('Eliseo Nava');
+}
+
+void main(){
+  // Eliseo Nava Mat: 22308051280777
+  // Llamar a la funcion saludar
+  print('Eliseo Nava Mat: 22308051280777');
+  print('Llamando a la funcion saludar');
+  saludar();
+  print('Llamando a la funcion escribeNombre');
+  escribeNombre();
+ 
+}
