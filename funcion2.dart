58267b14e8e8965e7e028ef4d6dabdcc7282ffa0
Manuel Nava <navaweb@cbtis128.edu.mx>
2025-02-10 18:04:23 +0000

    ejemplo funcion rama

---
+++ funcion2.dart
@@ -0,0 +1,15 @@
+// funciones creadas por el usuario
+void suma(int a, int b){
+  print('La suma es: ${a + b}');
+}
+
+void resta(int a, int b){
+  print('La resta es: ${a - b}');
+}
+// funcion main
+void main(){
+  print('Eliseo Nava Mat: 22308051280777');
+  print('Llamando a la funcion suma y resta');
+  suma(10, 5);
+  resta(10, 5);
+}
